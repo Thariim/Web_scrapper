3d14e0e37b0e8fc5fffe75f2d2ff4d214d174748
Viktor KRCHNAVY <viktor.krchnavy@cz.abb.com>
2024-10-02 11:04:46 +0200

    updated functions

---
+++ param_maker.py
@@ -0,0 +1,55 @@
+class ParamMaker:
+    def __init__(self, soup):
+        """
+        Initialize with a BeautifulSoup object.
+        
+        Args:
+            soup (BeautifulSoup): BeautifulSoup object containing the HTML content.
+        """
+        self.soup = soup
+
+    def number_maker(self, num):
+         """
+        Extract the number from the HTML.
+        
+        Returns:
+            int: Number of voters or None if not found.
+        """
+         try:
+            td_elements = self.soup.find_all("td", {'headers': num})
+            if td_elements:
+               return int(td_elements[0].text.strip().replace('\xa0', ''))
+            return None
+         except Exception as e:
+            print(f"An error occurred: {e}")
+            sys.exit(1)
+
+    def name_maker(self):
+        """
+        Extract the village name from the HTML.
+        
+        Returns:
+            str: Village name or None if not found.
+        """
+        target_value = 'Obec:'
+
+        return next(
+                    (h3.text.replace(target_value, '').strip()
+                    for h3 in self.soup.find_all("h3")
+                    if target_value in h3.text),
+                    None
+        )
+
+    def party_maker(self):
+        """
+        Extract the party names and their votes from the HTML.
+        
+        Returns:
+            dict: Dictionary of party names and their respective votes.
+        """
+        party_names = self.soup.find_all("td", {"class": "overflow_name"})
+        party_votes = self.soup.find_all("td", {"headers": "t1sa2 t1sb3"})
+
+        parties= [(name.text.strip() , int(vote.text.strip().replace('\xa0', '').replace(' ', ''))) for name, vote in zip(party_names, party_votes)]
+
+        return parties
